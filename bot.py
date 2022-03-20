8c8fd5d04982ebb6f8d6d096c0bd50b5c43078ad
oldjiang1101 <101724001+oldjiang1101@users.noreply.github.com>
2022-03-20 21:02:48 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
-from distutils import dep_util
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(common_prefix = '[')
+bot = commands.Bot(commands_prefix = '[')
 
 @bot.event
 async def on_ready():
